be1201b427ef5a536b649be22a9a4fbbb0a0381f
yangww <dayang_techmail@163.com>
2020-10-27 16:32:25 +0800

    1

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,7 @@
 git pull
 curpwd=$(pwd)
 cd ..
+docker rm $(docker ps -a -q)
 
 echo -e "\033[33m 是否启动consul\033[0m"
 echo "y 是"
@@ -8,7 +9,7 @@ echo "n 否"
 read start_con
 if [ $start_con == "y" ] 
 then
-    JOIN_IP="192.168.7.32"
+    JOIN_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' consul_server_1)";
     docker run -d -p 8500:8500 --restart=always -v /data/consul/data/server1:/consul/data -v /data/consul/conf/server1:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_1 consul:latest agent -server -bootstrap-expect=2 -ui -node=consul_server_1 -client='0.0.0.0' -data-dir /consul/data -config-dir /consul/config -datacenter=xdp_dc;
     docker run -d -p 8501:8500 --restart=always -v /data/consul/data/server2:/consul/data -v /data/consul/conf/server2:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_2 consul:latest agent -server -ui -node=consul_server_2 -client='0.0.0.0' -datacenter=xdp_dc -data-dir /consul/data -config-dir /consul/config -join=$JOIN_IP
     docker run -d -p 8502:8500 --restart=always -v /data/consul/data/server3:/consul/data -v /data/consul/conf/server3:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_3 consul:latest agent -server -ui -node=consul_server_3 -client='0.0.0.0' -datacenter=xdp_dc -data-dir /consul/data -config-dir /consul/config -join=$JOIN_IP
