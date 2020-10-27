a389dd33d594dc320922859a017a0d8737edad59
yangww <dayang_techmail@163.com>
2020-10-27 15:47:10 +0800

    1

--- build/run.sh
+++ build/run.sh
@@ -6,16 +6,13 @@ echo -e "\033[33m 是否启动consul\033[0m"
 echo "y 是"
 echo "n 否"
 read start_con
-if($start_con="y")
+if[$start_con=="y"]
 then
-do
 docker run -d -p 8500:8500 --restart=always -v /XiLife/consul/data/server1:/consul/data -v /XiLife/consul/conf/server1:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_1 consul:latest agent -server -bootstrap-expect=2 -ui -node=consul_server_1 -client='0.0.0.0' -data-dir /consul/data -config-dir /consul/config -datacenter=xdp_dc;
 
 docker run -d -p 8501:8500 --restart=always -v /XiLife/consul/data/server2:/consul/data -v /XiLife/consul/conf/server2:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_2 consul:latest agent -server -ui -node=consul_server_2 -client='0.0.0.0' -datacenter=xdp_dc -data-dir /consul/data -config-dir /consul/config -join=$JOIN_IP
 
 docker run -d -p 8502:8500 --restart=always -v /XiLife/consul/data/server3:/consul/data -v /XiLife/consul/conf/server3:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_3 consul:latest agent -server -ui -node=consul_server_3 -client='0.0.0.0' -datacenter=xdp_dc -data-dir /consul/data -config-dir /consul/config -join=$JOIN_IP
-
-done
 fi
 
 docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
