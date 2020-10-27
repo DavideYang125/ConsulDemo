d4cb400e1bf0c395da011891c172551eafcbc74d
yangww <dayang_techmail@163.com>
2020-10-27 14:33:23 +0800

    sh

--- build/run.sh
+++ build/run.sh
@@ -1,2 +1,8 @@
 git pull
-#docker run 
+curpwd=$(pwd)
+cd ..
+docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
+docker run -d -p 8580:80 serivceaimg:v1.0
+
+docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
+docker run -d -p 8580:80 serivceaimg:v1.0
