f942e2d7ac251b07d45f7904f6d1086f0b715490
yangww <dayang_techmail@163.com>
2020-10-27 14:34:44 +0800

    port

--- build/run.sh
+++ build/run.sh
@@ -5,4 +5,4 @@ docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
 docker run -d -p 8580:80 serivceaimg:v1.0
 
 docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
-docker run -d -p 8580:80 serivceaimg:v1.0
+docker run -d -p 8560:80 serivceaimg:v1.0
