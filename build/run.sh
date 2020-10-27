e7537bac7a8e9e03ec6d70bd330925d28d1af8c8
yangww <dayang_techmail@163.com>
2020-10-27 14:37:32 +0800

    1

--- build/run.sh
+++ build/run.sh
@@ -4,5 +4,5 @@ cd ..
 docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
 docker run -d -p 8580:80 serivceaimg:v1.0
 
-docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
-docker run -d -p 8560:80 serivceaimg:v1.0
+docker build -f ./src/ServiceB/Dockerfile -t serivcebimg:v1.0 .
+docker run -d -p 8560:80 serivcebimg:v1.0
