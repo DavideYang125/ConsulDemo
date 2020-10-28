e8f2e2b1686cc22035e7b78cdff1c90c4a5898b5
yangww <dayang_techmail@163.com>
2020-10-28 09:59:40 +0800

    build

--- build/run.sh
+++ build/run.sh
@@ -17,10 +17,20 @@ then
     docker run -d -p 8502:8500 --restart=always -v /data/consul/data/server3:/consul/data -v /data/consul/conf/server3:/consul/config -e CONSUL_BIND_INTERFACE='eth0' --privileged=true --name=consul_server_3 consul:latest agent -server -ui -node=consul_server_3 -client='0.0.0.0' -datacenter=dc1 -data-dir /consul/data -config-dir /consul/config -join=$JOIN_IP
 fi
 
-docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
-docker run -d -p 8580:80 serivceaimg:v1.0
+echo -e "\033[33m 是否启动consul\033[0m"
+echo "y 是"
+echo "n 否"
+read start_con
+if [ $start_con == "y" ]
+then
+    docker stop $(docker ps | grep serivce | awk '{print $1}')
+    docker rm $(docker ps -qf status=exited)
 
-docker build -f ./src/ServiceB/Dockerfile -t serivcebimg:v1.0 .
-docker run -d -p 8560:80 serivcebimg:v1.0
+    docker build -f ./src/ServiceA/Dockerfile -t serivceaimg:v1.0 .
+    docker run -d -p 8580:80 serivceaimg:v1.0
+
+    docker build -f ./src/ServiceB/Dockerfile -t serivcebimg:v1.0 .
+    docker run -d -p 8560:80 serivcebimg:v1.0
+fi
 
 docker image prune -f
